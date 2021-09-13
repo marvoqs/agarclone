1ac151fca13f4266370b54cecabe50afb6c93dd6
Marek Schwarz <marek.schwarz@designdev.cz>
2021-09-13 14:38:26 +0200

    Init project

---
+++ servers.js
@@ -0,0 +1,16 @@
+// servers.js is only for the making of the socketio server and the express server
+const express = require("express");
+const app = express();
+app.use(express.static(__dirname + "/public"));
+const socketio = require("socket.io");
+const expressServer = app.listen(8080);
+const io = socketio(expressServer);
+const helmet = require("helmet");
+app.use(helmet());
+console.log("express and socketio are listening on port 8080");
+
+// App organization
+module.exports = {
+  app,
+  io,
+};
